0b5bf30ce74de183bed0fb66077785239747dbd1
akulkina.adelya <akulkina.adelya@wb.ru>
2022-12-24 03:55:27 +0400

    add sizing

--- mosaic_server/server.go
+++ mosaic_server/server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"image/png"
 	"io/ioutil"
+	"log"
 	"main/color_mosaic"
 	"main/image_processing"
 	"net/http"
@@ -21,9 +22,13 @@ func (s *Server) Load() {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 	r.POST("/mosaic", func(c *gin.Context) {
-		formFile, _ := c.FormFile("img")
+		formFile, err := c.FormFile("img")
+		if err != nil {
+			log.Fatal(err)
+		}
 		var mzk color_mosaic.Mosaic
 		chose := c.Request.Form.Get("Mosaic")
+		size := c.Request.Form.Get("size")
 		switch chose {
 		case "grayscale":
 			mzk = color_mosaic.NewGrayscaleMosaic(3)
@@ -38,10 +43,27 @@ func (s *Server) Load() {
 		default:
 			mzk = color_mosaic.NewGrayscaleMosaic(3)
 		}
-		openedFile, _ := formFile.Open()
-		file, _ := ioutil.ReadAll(openedFile)
+		openedFile, err := formFile.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		file, err := ioutil.ReadAll(openedFile)
+		if err != nil {
+			log.Fatal(err)
+		}
 		var im image_processing.Image
 		im.LoadImageBytes(file)
+		switch size {
+		case "original":
+		case "1000x1000":
+			im = im.ResizeImage(1000, 1000)
+		case "500x500":
+			im = im.ResizeImage(500, 500)
+		case "250x250":
+			im = im.ResizeImage(250, 250)
+		default:
+
+		}
 		res := mzk.MakeMosaic(im)
 		buf := new(bytes.Buffer)
 		png.Encode(buf, res)
