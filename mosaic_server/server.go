ada924b9d92edeaa22d72d9ad69e134b365e54d7
akulkina.adelya <akulkina.adelya@wb.ru>
2022-12-24 03:07:26 +0400

    add choosing mosaic type in html

--- mosaic_server/server.go
+++ mosaic_server/server.go
@@ -22,9 +22,24 @@ func (s *Server) Load() {
 	})
 	r.POST("/mosaic", func(c *gin.Context) {
 		formFile, _ := c.FormFile("img")
+		var mzk color_mosaic.Mosaic
+		chose := c.Request.Form.Get("Mosaic")
+		switch chose {
+		case "grayscale":
+			mzk = color_mosaic.NewGrayscaleMosaic(3)
+		case "blue":
+			mzk = color_mosaic.NewBlueMosaic()
+		case "red":
+			mzk = color_mosaic.NewRedMosaic()
+		case "green":
+			mzk = color_mosaic.NewGreenMosaic()
+		case "peach":
+			mzk = color_mosaic.NewPeachMosaic()
+		default:
+			mzk = color_mosaic.NewGrayscaleMosaic(3)
+		}
 		openedFile, _ := formFile.Open()
 		file, _ := ioutil.ReadAll(openedFile)
-		mzk := color_mosaic.NewPeachMosaic()
 		var im image_processing.Image
 		im.LoadImageBytes(file)
 		res := mzk.MakeMosaic(im)
